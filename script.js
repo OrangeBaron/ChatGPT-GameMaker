e69acb0329549adee3d8562ff80c8421459a3ae7
Francesco Focher <francescofocher@gmail.com>
2025-04-11 00:33:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -196,7 +196,7 @@
 
   // Al caricamento, carica la configurazione, prepara la UI e imposta i listener
   window.addEventListener("load", async () => {
-    await delay(30000);
+    await delay(3000);
     const config = await loadGameConfig();
     loadedParams = {
       prompt_start: config["Prompt iniziale"],
